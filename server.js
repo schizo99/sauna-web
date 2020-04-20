1542022f7fe9e99a0397dde8d20903013f7b2ee2
Andreas Åhman <andreas.ahman@ingka.ikea.com>
2020-04-20 15:33:14 +0200

    Fix return

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(history({ index: '/index.html' }));
 
 app.get('/api/*', (req, res) => {
-  const api = req.url.split('/api');
+  const api = req.url.split('/api').slice(1).join('');
   const host = process.env.API_HOST || 'localhost';
-  axios.get(`http://${host}:3000${api[1]}`)
-    .then((result) => res.json(result))
+  axios.get(`http://${host}:3000${api}`)
+    .then((result) => res.json(result.data))
     .catch(() => {
       console.log('Unable to reach API server!');
       res.status(400);
