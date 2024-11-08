46d5497ee14fd64e99118a8656084d3721b2bf90
Andreas Åman <andreas.aman@atg.se>
2024-11-08 09:41:06 +0100

    Move stuff to api

--- entry.js
+++ entry.js
@@ -2,55 +2,14 @@ import {server as app} from "./build/index.js"
 import cron from "node-cron";
 
 
-async function updateTelegramDate() {
-    const req = await fetch('http://0.0.0.0:3000/api/telegram', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': process.env.AUTHENTICATION
-        },
-        body: JSON.stringify({ date: new Date().toISOString() })
-    });
-    let result = await req;
-    if (!result.ok) {
-        console.error('Failed to update telegram date', result);
-    }
-}
-
 async function sendSignal() {
-    const req = await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            chat_id: process.env.CHAT_ID,
-            text: `Sauna temp has passed: 75°C`
-        })
-    });
-    const json = await req.json()
-    if (json.ok) {
-        updateTelegramDate();
-    } else {
-        console.error('Message failed to send', json);
-    }
-}
-
-
-async function checkDate() {
     const req = await fetch(`http://0.0.0.0:3000/api/telegram`, {
-        method: 'GET',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': process.env.AUTHENTICATION
         }
     });
-    const date = await req.json();
-    const sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
-    const telegramDate = new Date(date[0].telegram);
-    if (telegramDate < sixHoursAgo) {
-        sendSignal();
-    }
 }
 
 const cronTask = cron.schedule("*/15 * * * * *", async () => {
@@ -64,8 +23,8 @@ const cronTask = cron.schedule("*/15 * * * * *", async () => {
         });
         const temp = await req.json();
         if (temp[0].temp > 7500) {
-            console.log("Temp is over 80 degrees");
-            checkDate();
+            console.log(new Date().toISOString(), "Temp is over 75 degrees");
+            sendSignal();
         }
     } catch (error) {
         console.error("Error checking temp:", error);
--- src/routes/api/telegram/+server.js
+++ src/routes/api/telegram/+server.js
@@ -1,28 +1,51 @@
 import influx from '$lib/telegram';
 
-export async function POST({request}) {
-    const { date } = await request.json();
-    let result = await influx.writePoints([
-        {
-          measurement: 'telegram',
-          fields: { date },
-        }
-      ])
-      .catch(err => {
-        console.log(err)
-        return "bajs"
-    })
-
-    return new Response(JSON.stringify(result))
+export async function POST() {
+  let result = await influx.query(`select last("date") as telegram from telegram`).catch(err => {
+    console.log(err)
+    return "bajs"
+  })
+  const date = await result;
+  const sixHoursAgo = new Date(Date.now() - 6 * 60 * 60 * 1000);
+  const telegramDate = new Date(date[0].telegram);
+  if (telegramDate < sixHoursAgo) {
+    sendSignal();
+  }
+  return new Response("OK")
 }
 
-export async function GET() {
-  let result = await influx.query(`
-  select last("date") as telegram from telegram
-`).catch(err => {
-      console.log(err)
-      return "bajs"
-  })
+async function updateTelegramDate() {
+  try {
+    const date = new Date().toISOString();
+    await influx.writePoints([
+      {
+        measurement: 'telegram',
+        fields: { date },
+      }
+    ])
+  } catch (err) {
+    console.log("Failed setting telegramdate", err)
+    return "bajs"
+  }
+}
 
-  return new Response(JSON.stringify(result))
+
+async function sendSignal() {
+  const req = await fetch(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      chat_id: process.env.CHAT_ID,
+      text: `Sauna temp has passed: 75°C`
+    })
+  });
+  const result = await req;
+
+  if (result.ok) {
+    updateTelegramDate();
+  } else {
+    console.error('Message failed to send', result);
+  }
 }
