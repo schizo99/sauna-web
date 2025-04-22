44f62f93e63dcf1db5e82402ad98659b498edd7e
Andreas Åman <andreas.aman@atg.se>
2025-04-22 13:14:57 +0200

    Provie OG information

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	define: {
+		'import.meta.env.BASE_URL': JSON.stringify('https://sauna.schizo.eu/'),
+	  },
 	plugins: [sveltekit()]
 });
