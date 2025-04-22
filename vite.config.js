5048ce5028ea0f8d2803403c3163f599cfb61633
Andreas Åman <andreas.aman@atg.se>
2025-04-22 13:28:25 +0200

    Add slash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	define: {
-		'import.meta.env.BASE_URL': JSON.stringify('https://sauna.schizo.eu/'),
+		'import.meta.env.BASE_URL': JSON.stringify('https://sauna.schizo.eu'),
 	  },
 	plugins: [sveltekit()]
 });
